6813c9f6fcff8f3f148f684d9b88cc5bf60eed90
vasamoylov <vfea-vitalik@mail.ru>
2025-06-30 08:44:22 +0300

    Add files via upload

--- api.py
+++ api.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel, EmailStr
 from pydantic_extra_types.phone_numbers import PhoneNumber
 from typing import List
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, insert, Column, Integer, String, MetaData
 from sqlalchemy.orm import sessionmaker, Session
-from models import Base, Clients, Subscriptions, Services
+from models import Base, Clients, Subscriptions, Services, ClientSubscriptions, ServicesAvailability, Categories
+from datetime import date
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -27,10 +28,24 @@ class SubscriptionCreate(BaseModel):
     price: int
 
 
+class CategoryCreate(BaseModel):
+    name: str
+
+
 class ServiceCreate(BaseModel):
-    id_subscription: int
+    category_id: int
     description: str
     price: int
+    subscription_id: int
+    quantity: int
+
+
+class ClientSubscriptionCreate(BaseModel):
+    client_id: int
+    subscription_id: int
+    start_date: date
+    end_date: date
+    payment_status: str
 
 
 class ClientResponse(ClientCreate):
@@ -38,6 +53,10 @@ class ClientResponse(ClientCreate):
 
 
 class SubscriptionResponse(SubscriptionCreate):
+    id: int
+
+
+class CategoryResponse(CategoryCreate):
     id: int
 
 
@@ -68,39 +87,45 @@ async def create_client(client: ClientCreate, db: Session = Depends(get_db)):
     return db_client
 
 
-@app.get('/clients/', response_model=List[ClientResponse])
+@app.get('/clients/')
 async def get_clients(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    total_clients = db.query(Clients).count()
     clients = db.query(Clients).offset(skip).limit(limit).all()
-    return clients
+    return {
+        'total': total_clients,
+        'skip': skip,
+        'limit': limit,
+        'clients': clients
+    }
 
 
 @app.get('/clients/{id}', response_model=ClientResponse)
-async def get_contact(client_id: int, db: Session = Depends(get_db)):
+async def get_client(client_id: int, db: Session = Depends(get_db)):
     client = db.get(Clients, client_id)
     if not client:
-        raise HTTPException(status_code=404, detail="Contact not found")
+        raise HTTPException(status_code=404, detail='Client not found')
     return client
 
 
 @app.put('/clients/{id}', response_model=ClientResponse)
-async def update_contact(contact_id: int, client: ClientCreate, db: Session = Depends(get_db)):
-    db_client = db.get(Clients, contact_id)
+async def update_client(client_id: int, client: ClientCreate, db: Session = Depends(get_db)):
+    db_client = db.get(Clients, client_id)
     if not db_client:
-        raise HTTPException(status_code=404, detail="Contact not found")
+        raise HTTPException(status_code=404, detail='Client not found')
     db_client.name = client.name
     db.commit()
     db.refresh(db_client)
     return db_client
 
 
-@app.delete('/contacts/{id}')
-async def delete_contact(client_id: int, db: Session = Depends(get_db)):
+@app.delete('/clients/{id}')
+async def delete_client(client_id: int, db: Session = Depends(get_db)):
     db_client = db.get(Clients, client_id)
     if not db_client:
-        raise HTTPException(status_code=404, detail="Contact not found")
+        raise HTTPException(status_code=404, detail='Client not found')
     db.delete(db_client)
     db.commit()
-    return {"message": "Contact deleted"}
+    return {'message': 'Client deleted'}
 
 
 @app.post('/subscriptions/', response_model=SubscriptionResponse)
@@ -112,10 +137,177 @@ async def create_subscription(subscription: SubscriptionCreate, db: Session = De
     return db_subscription
 
 
-@app.post('/subscriptions/services/', response_model=ServiceResponse)
+@app.get('/subscriptions/')
+async def get_subscriptions(db: Session = Depends(get_db)):
+    subscriptions = db.query(Subscriptions).all()
+    return subscriptions
+
+
+@app.put('/subscriptions/{id}', response_model=SubscriptionResponse)
+async def get_subscription(subscription_id, subscription: SubscriptionCreate, db: Session = Depends(get_db)):
+    db_subscription = db.get(Subscriptions, subscription_id)
+    if not db_subscription:
+        raise HTTPException(status_code=404, detail='Subscription not found')
+    db_subscription.name = subscription.name
+    db_subscription.price = subscription.price
+    db.commit()
+    db.refresh(db_subscription)
+    return db_subscription
+
+
+@app.delete('/subscriptions/{id}')
+async def delete_subscription(subscription_id: int, db: Session = Depends(get_db)):
+    db_subscription = db.get(Subscriptions, subscription_id)
+    if not db_subscription:
+        raise HTTPException(status_code=404, detail='Subscription not found')
+    db.delete(db_subscription)
+    db.commit()
+    return {'message': 'Subscription deleted'}
+
+
+@app.post('/categories/', response_model=CategoryResponse)
+async def create_category(category: CategoryCreate, db: Session = Depends(get_db)):
+    db_category = Categories(**category.dict())
+    db.add(db_category)
+    db.commit()
+    db.refresh(db_category)
+    return db_category
+
+
+@app.get('/categories/')
+async def get_categories(db: Session = Depends(get_db)):
+    categories = db.query(Categories).all()
+    return categories
+
+
+@app.put('/categories/{id}', response_model=CategoryResponse)
+async def get_category(category_id, category: CategoryCreate, db: Session = Depends(get_db)):
+    db_category = db.get(Categories, category_id)
+    if not db_category:
+        raise HTTPException(status_code=404, detail='Category not found')
+    db_category.name = category.name
+    db.commit()
+    db.refresh(db_category)
+    return db_category
+
+
+@app.delete('/categories/{id}')
+async def delete_category(category_id: int, db: Session = Depends(get_db)):
+    db_category = db.get(Categories, category_id)
+    if not db_category:
+        raise HTTPException(status_code=404, detail='Categoryn not found')
+    db.delete(db_category)
+    db.commit()
+    return {'message': 'Category deleted'}
+
+
+@app.post('/services/', response_model=ServiceResponse)
 async def create_service(service: ServiceCreate, db: Session = Depends(get_db)):
     db_service = Services(**service.dict())
     db.add(db_service)
     db.commit()
     db.refresh(db_service)
     return db_service
+
+
+@app.get('/services/')
+async def get_services(db: Session = Depends(get_db)):
+    services = db.query(Services).all()
+    return services
+
+
+@app.put('/services/{id}', response_model=ServiceResponse)
+async def get_service(service_id, service: ServiceCreate, db: Session = Depends(get_db)):
+    db_service = db.get(Services, service_id)
+    if not db_service:
+        raise HTTPException(status_code=404, detail='Service not found')
+    db_service.description = service.description
+    db_service.price = service.price
+    db.commit()
+    db.refresh(db_service)
+    return db_service
+
+
+@app.delete('/services/{id}')
+async def delete_service(service_id: int, db: Session = Depends(get_db)):
+    db_service = db.get(Services, service_id)
+    if not db_service:
+        raise HTTPException(status_code=404, detail='Service not found')
+    db.delete(db_service)
+    db.commit()
+    return {'message': 'Service deleted'}
+
+
+@app.post('/clients/{id}/subscription/')
+async def create_client_subscription(client_subscription: ClientSubscriptionCreate, db: Session = Depends(get_db)):
+    db_client_subscription = ClientSubscriptions(**client_subscription.dict())
+    db.add(db_client_subscription)
+    service_availability = (
+        db.query(
+            Services.id, Services.quantity).filter_by(subscription_id=db_client_subscription.subscription_id).all()
+    )
+    for service in service_availability:
+        db_service = ServicesAvailability(db_client_subscription.client_id, service.id, service.quantity)
+        db.add(db_service)
+    db.commit()
+    db.refresh(db_client_subscription)
+    return {'message': 'Subscription completed successfully'}
+
+
+@app.get('/clients/{id}/subscription/')
+async def get_client_subscription(client_id: int, db: Session = Depends(get_db)):
+    client = db.get(ClientSubscriptions, client_id)
+    if not client:
+        raise HTTPException(status_code=404, detail='Client not have subscription')
+    else:
+        client_subscription = db.query(ClientSubscriptions).filter_by(client_id=client_id).one()
+    return client_subscription
+
+
+@app.put('/clients/{id}/subscription/', response_model=ClientResponse)
+async def update_client_subscription(client_id: int, client_subscription: ClientSubscriptionCreate,
+                                     db: Session = Depends(get_db)):
+    db_client = db.get(ClientSubscriptions, client_id)
+    if not db_client:
+        raise HTTPException(status_code=404, detail='Client not found')
+    db_client.subscription_id = client_subscription.subscription_id
+    db.commit()
+    db.refresh(db_client)
+    return db_client
+
+
+@app.delete('/clients/{id}/subscription/')
+async def delete_client_subscription(client_subscription_id: int, db: Session = Depends(get_db)):
+    db_client_subscription = db.get(ClientSubscriptions, client_subscription_id)
+    if not db_client_subscription:
+        raise HTTPException(status_code=404, detail='Client subscription not found')
+    db.delete(db_client_subscription)
+    db.commit()
+    return {'message': 'Client subscription deleted'}
+
+
+@app.put('/clients/{id}/subscription/services', response_model=ClientResponse)
+async def use_availability_services(client_id: int, service_id: int, quantity: int, db: Session = Depends(get_db)):
+    db_availability_quantity = (
+        db.query(ServicesAvailability.quantity).filter_by(client_id=client_id, service_id=service_id)).one()
+    if not db_availability_quantity:
+        raise HTTPException(status_code=404, detail='This service is not included in the subscription')
+    db_availability_quantity.quantity = db_availability_quantity.quantity - quantity
+    db.commit()
+    return {'message': 'The service was successfully used'}
+
+
+# from sqlalchemy import MetaData
+
+# metadata = MetaData()
+# metadata.reflect(bind=engine)
+# my_table = metadata.tables['alembic_version']
+# my_table.drop(engine)
+
+'''
+session = Session(bind=engine)
+data = session.query(ServicesAvailability.quantity).filter_by(client_id=1, service_id=2).one()
+
+print(type(data.quantity))'''
+
+
--- models.py
+++ models.py
@@ -1,12 +1,12 @@
 from sqlalchemy.orm import declarative_base, relationship
-from sqlalchemy import Column, Integer, String, Date, ForeignKey, CheckConstraint
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, CheckConstraint, Enum
 
 Base = declarative_base()
 
 
 class Clients(Base):
     __tablename__ = 'clients'
-    # Таблица с личными данными клиентов
+    '''Таблица с личными данными клиентов'''
 
     __table_args__ = (
         CheckConstraint('age > 0', name='check_age'),
@@ -16,29 +16,32 @@ class Clients(Base):
     surname = Column(String(25))
     patronymic = Column(String(25))
     age = Column(Integer)
-    phone = Column(String(15), nullable=False, unique=True)
-    email = Column(String(20))
-    client_subscriptions = relationship('ClientSubscriptions', back_populates='client')
-    services_availability = relationship('ServicesAvailability', back_populates='client')
+    phone = Column(String(15), nullable=False, unique=True, index=True)
+    email = Column(String(50))
+    client_subscription = relationship('ClientSubscriptions', back_populates='client',
+                                       cascade='all, delete-orphan')
+    services_availability = relationship('ServicesAvailability', back_populates='client',
+                                         cascade='all, delete-orphan')
 
 
 class Subscriptions(Base):
     __tablename__ = 'subscriptions'
-    # Таблица с наименованием подписок
+    '''Таблица с наименованием подписок'''
 
     __table_args__ = (
-        CheckConstraint('price > 0', name='check_price'),
+        CheckConstraint('price >= 0', name='check_price'),
     )
     id = Column(Integer, primary_key=True)
-    name = Column(String(10), nullable=False)
+    name = Column(String(30), nullable=False)
     price = Column(Integer, nullable=False)
-    client_subscriptions = relationship('ClientSubscriptions', back_populates='subscription')
-    services = relationship('Services', back_populates='subscription')
+    client_subscription = relationship('ClientSubscriptions', back_populates='subscription',
+                                       cascade='all, delete-orphan')
+    included_services = relationship('Services', back_populates='subscriptions')
 
 
 class ClientSubscriptions(Base):
     __tablename__ = 'client_subscriptions'
-    # Таблица с информацией о подписках клиентов
+    '''Таблица с информацией о подписках клиентов'''
 
     __table_args__ = (
         CheckConstraint('start_date < end_date', name='check_date'),
@@ -48,29 +51,43 @@ class ClientSubscriptions(Base):
     subscription_id = Column(Integer, ForeignKey('subscriptions.id'))
     start_date = Column(Date)
     end_date = Column(Date)
-    payment_status = Column(String(10), nullable=False)
-    client = relationship('Clients', back_populates='client_subscriptions')
-    subscription = relationship('Subscriptions', back_populates='client_subscriptions')
+    payment_status = Column(Enum('paid', 'not_paid'))
+    client = relationship('Clients', back_populates='client_subscription')
+    subscription = relationship('Subscriptions', back_populates='client_subscription')
+
+
+class Categories(Base):
+    __tablename__ = 'categories'
+    '''Таблица с категориями стоматологических услуг (ортопедия, хирургия и т.п.)'''
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30), nullable=False)
+    services = relationship('Services', back_populates='category',
+                            cascade='all, delete-orphan')
 
 
 class Services(Base):
     __tablename__ = 'services'
-    # Таблица с перечнем услуг и ценами, которые входят в подписки
+    '''Таблица с перечнем услуг и ценами, в т.ч. которые входят в подписки'''
 
     __table_args__ = (
         CheckConstraint('price > 0', name='check_price'),
+        CheckConstraint('price > 0', name='check_price'),
     )
     id = Column(Integer, primary_key=True)
-    id_subscription = Column(Integer, ForeignKey('subscriptions.id'))
+    category_id = Column(Integer, ForeignKey('categories.id'))
     description = Column(String(50), nullable=False)
     price = Column(Integer, nullable=False)
+    subscription_id = Column(Integer, ForeignKey('subscriptions.id'))
+    quantity = Column(Integer)
     services_availability = relationship('ServicesAvailability', back_populates='service')
-    subscription = relationship('Subscriptions', back_populates='services')
+    subscriptions = relationship('Subscriptions', back_populates='included_services')
+    category = relationship('Categories', back_populates='services')
 
 
 class ServicesAvailability(Base):
     __tablename__ = 'services_availability'
-    # Таблица с информацией о количестве доступных улуг для клиента в рамках подписки
+    '''Таблица с информацией о количестве доступных улуг для клиента в рамках подписки'''
 
     __table_args__ = (
         CheckConstraint('quantity >= 0', name='check_quantitye'),
@@ -85,7 +102,7 @@ class ServicesAvailability(Base):
 
 class Contacts(Base):
     __tablename__ = 'contacts'
-    # Таблица с контактами
+    '''Таблица с контактами клиники'''
 
     id = Column(Integer, primary_key=True)
     name = Column(String(10), nullable=False)
@@ -94,32 +111,9 @@ class Contacts(Base):
 
 class Offers(Base):
     __tablename__ = 'offers'
-    # Таблица с оферами
+    '''Таблица с оферами'''
 
     id = Column(Integer, primary_key=True)
     name = Column(String(20), nullable=False)
     url = Column(String(30), nullable=False)
-
-
-class Categories(Base):
-    __tablename__ = 'categories'
-    # Таблица с категориями стоматологических услуг (ортопедия, хирургия и т.п.)
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(10), nullable=False)
-    service = relationship('PriceList', back_populates='category')
-
-
-class PriceList(Base):
-    __tablename__ = 'price_list'
-    # Прайс лист всех стоматологических услуг
-
-    __table_args__ = (
-        CheckConstraint('price > 0', name='check_price'),
-    )
-    id = Column(Integer, primary_key=True)
-    category_id = Column(String(10), ForeignKey('categories.id'))
-    description = Column(String(50), nullable=False)
-    price = Column(Integer, nullable=False)
-    category = relationship('Categories', back_populates='service')
 
